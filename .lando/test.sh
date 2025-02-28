a9ceab6865bfd847559b478286b3f4958f41bacd
jmf3658 <jfullmer@austin.utexas.edu>
2025-02-28 13:44:25 -0700

    Add tooling for local development with Lando (#34)

---
+++ .lando/test.sh
@@ -0,0 +1,11 @@
+# We need to add drupal/core-dev for PHPUnit
+# We do this in a check here so as not to run `composer require`
+# every time we want to run tests.
+if ! grep -q "drupal/core-dev" composer.json; then
+  echo "Adding core tooling. This may take a moment...";
+  composer require "drupal/core-dev-pinned"
+fi
+mkdir -p /app/web/sites/simpletest/browser_output
+chmod 777 /app/web/sites/simpletest/browser_output
+# Now we actually run the command...
+/app/vendor/bin/phpunit -c /app/.lando/phpunit.xml --stop-on-failure --display-deprecations --testdox $1
